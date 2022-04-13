9467a2cba9c0a50847e2d03ca61471ae5cb0e395
Yitzchak Grinboum <yitzchakg@campus.technion.ac.il>
2022-04-13 13:27:15 +0300

    hello ~world~

---
+++ build/CMakeFiles/Matam.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Matam.dir/main.c.obj"
+  "CMakeFiles/Matam.dir/main.c.obj.d"
+  "Matam.exe"
+  "Matam.exe.manifest"
+  "Matam.pdb"
+  "libMatam.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Matam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Matam.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Matam.
+# This may be replaced when dependencies are built.
