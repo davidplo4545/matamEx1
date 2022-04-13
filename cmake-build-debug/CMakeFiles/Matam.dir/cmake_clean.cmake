9f566f3861f988e21ec36556735616492a0cc4b2
david plotkin <davidplo4545@gmail.com>
2022-04-13 13:11:42 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/Matam.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Matam.dir/main.c.obj"
+  "Matam.exe"
+  "Matam.exe.manifest"
+  "Matam.pdb"
+  "libMatam.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Matam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
